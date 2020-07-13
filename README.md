32354ee9fb81a1b378bc54701d6aa5076e0aaf31
Shehan <dmcshehan@outlook.com>
2020-07-13 14:31:17 +0530

    change date and edit readme

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
 ## Static Music Show Website
 
-If To launch this on your development invironment:
-
-Clone the project
-
-```js
-git clone https://github.com/dmcshehan/venue-website
-```
-
-Nagite to the folder
-
-```js
-cd venue-website
-```
-
-Install dependencies
-
-```js
-npm install
-```
-
-Start
-
-```js
-npm start
-```
+[Live Demo](https://music-event-website.netlify.app/)
--- src/components/featured/TimeUnit.js
+++ src/components/featured/TimeUnit.js
@@ -1,98 +1,74 @@
-import React, { Component } from 'react';
-import Slide from 'react-reveal/Slide';
+import React, { Component } from "react";
+import Slide from "react-reveal/Slide";
 
 class TimeUnit extends Component {
-    state = {
-        deadline: 'Dec, 16, 2019',
-        days: '0',
-        hours: '0',
-        minutes: '0',
-        seconds: '0'
+  state = {
+    deadline: "Dec, 16, 2022",
+    days: "0",
+    hours: "0",
+    minutes: "0",
+    seconds: "0",
+  };
 
-    };
+  addZeros(val) {
+    return val.toString().length === 1 ? `0${val}` : val;
+  }
 
-    addZeros(val) {
-        return val.toString().length === 1 ? `0${val}` : val;
-    }
+  gettime(deadline) {
+    const endDate = new Date(deadline);
+    const now = new Date();
+    const daysDiff = endDate - now;
+    const daysDiffInSec = daysDiff / 1000;
 
+    let seconds = this.addZeros(Math.floor(daysDiffInSec % 60));
+    let minutes = this.addZeros(Math.floor((daysDiffInSec / 60) % 60));
+    const hours = this.addZeros(Math.floor((daysDiffInSec / (60 * 60)) % 24));
+    const days = this.addZeros(Math.floor(daysDiffInSec / (3600 * 24)));
 
-    gettime(deadline) {
-        const endDate = new Date(deadline);
-        const now = new Date();
-        const daysDiff = endDate - now;
-        const daysDiffInSec = daysDiff / 1000;
+    this.setState({
+      ...this.state,
+      days,
+      hours,
+      minutes,
+      seconds,
+    });
 
-        let seconds = this.addZeros(Math.floor(daysDiffInSec % 60));
-        let minutes = this.addZeros(Math.floor((daysDiffInSec / 60) % 60));
-        const hours = this.addZeros(Math.floor(daysDiffInSec / (60 * 60) % 24));
-        const days = this.addZeros(Math.floor(daysDiffInSec / (3600 * 24)));
+    // if seconds are divided by 60 it gives minutes if you use % it gives the remainder after divided into minutes which is remaining reconds
+  }
 
-        this.setState({
-            ...this.state,
-            days,
-            hours,
-            minutes,
-            seconds
-        });
+  componentDidMount() {
+    setInterval(() => {
+      this.gettime(this.state.deadline);
+    }, 1000);
+  }
 
-        // if seconds are divided by 60 it gives minutes if you use % it gives the remainder after divided into minutes which is remaining reconds
-    }
-
-
-    componentDidMount() {
-        setInterval(() => {
-            this.gettime(this.state.deadline);
-        }, 1000)
-    }
-
-
-    render() {
-        return (
-            <Slide left delay={1000}>
-                <div className="countdown_wrapper">
-                    <div className="countdown_top">
-                        Event Starts In
-                </div>
-                    <div className="countdown_bottom">
-                        <div className="countdown_item">
-                            <div className="countdown_time">
-                                {this.state.days}
-                            </div>
-                            <div className="countdown_tag">
-                                Days
-                        </div>
-                        </div>
-                        <div className="countdown_item">
-                            <div className="countdown_time">
-                                {this.state.hours}
-                            </div>
-                            <div className="countdown_tag">
-                                hrs
-                        </div>
-                        </div>
-                        <div className="countdown_item">
-                            <div className="countdown_time">
-                                {this.state.minutes}
-                            </div>
-                            <div className="countdown_tag">
-                                Min
-                        </div>
-                        </div>
-                        <div className="countdown_item">
-                            <div className="countdown_time">
-                                {this.state.seconds}
-                            </div>
-                            <div className="countdown_tag">
-                                Sec
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            </Slide>
-
-
-        );
-    }
+  render() {
+    return (
+      <Slide left delay={1000}>
+        <div className='countdown_wrapper'>
+          <div className='countdown_top'>Event Starts In</div>
+          <div className='countdown_bottom'>
+            <div className='countdown_item'>
+              <div className='countdown_time'>{this.state.days}</div>
+              <div className='countdown_tag'>Days</div>
+            </div>
+            <div className='countdown_item'>
+              <div className='countdown_time'>{this.state.hours}</div>
+              <div className='countdown_tag'>hrs</div>
+            </div>
+            <div className='countdown_item'>
+              <div className='countdown_time'>{this.state.minutes}</div>
+              <div className='countdown_tag'>Min</div>
+            </div>
+            <div className='countdown_item'>
+              <div className='countdown_time'>{this.state.seconds}</div>
+              <div className='countdown_tag'>Sec</div>
+            </div>
+          </div>
+        </div>
+      </Slide>
+    );
+  }
 }
 
 export default TimeUnit;
